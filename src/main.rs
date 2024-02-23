c35cbcd314e701af5d8ec7ac33910edff2a0b4af
Abdulqudduus Babalola <babalolajnr@gmail.com>
2024-02-23 19:14:30 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use axum::{routing::get, serve, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    serve(listener, app).await.unwrap();
+}
